91f4618e1aa044644e6f802c97c4a43574f5eaf9
RicePandaaaa <anthony.ha.pham@gmail.com>
2026-01-12 23:21:17 -0600

    Start on checklist

--- main.py
+++ main.py
@@ -6,7 +6,10 @@ import io
 st.sidebar.markdown("## Table of Contents")
 st.sidebar.markdown("[VTT to Plain Text](#vtt-to-plain-text)")
 
-# Main content with sections
+"""
+VTT to Plain Text
+=================
+"""
 st.markdown('<div id="vtt-to-plain-text"></div>', unsafe_allow_html=True)
 st.header("VTT to Plain Text")
 
@@ -37,3 +40,40 @@ if uploaded_file is not None:
         st.error("Error converting file. Please try again.")
 else:
     st.info("Please upload a VTT file to convert to a plain text file.")
+
+"""
+Video Transcription Checklist
+=============================
+"""
+
+# Initialize session state for these checklists
+checklist_keys: list[str] = [
+    "vimeo_processed",
+    "first_frame_processed",
+    "closed_captioning_processed",
+    "transcript_processed",
+    "vimeo_link_working",
+    "video_download_available",
+    "transcript_download_available"
+]
+
+
+st.markdown("""
+            Below is a rough checklist to make sure all videos are processed correctly for Canvas usage.
+            """)
+
+# Create the checklist
+st.checkbox("Video is processed on Vimeo",
+            key="vimeo_processed")
+st.checkbox("Video's first frame is just the intro slide along with the webcam(s) of the presenter(s)",
+            key="first_frame_processed")
+st.checkbox("Video has closed captioning (from Rev)",
+            key="closed_captioning_processed")
+st.checkbox("Transcript has been formed with screenshots from the video and the closed captions",
+            key="transcript_processed")
+st.checkbox("player.vimeo.com/video/<video_id> is working correctly",
+            key="vimeo_link_working")
+st.checkbox("Video is available for download",
+            key="video_download_available")
+st.checkbox("Transcript is available for download",
+            key="transcript_download_available")
